fd6510816667266c238863d7a7e03b6466cac385
dipesh4036 <111883699+dipesh4036@users.noreply.github.com>
2023-10-07 08:54:53 +0000

    main

---
+++ Basic-1/02_Operators.java
@@ -0,0 +1,18 @@
+class Opreators{
+    public static void main(String[] args) {
+        
+//Arithmetic Operators:-
+        // 1) Binary :-  +, - , * , / , %
+        // 2) Unary :-  ++ , --
+            // Ex   ++a :- pre Increment.  , a++ :- Post Increment
+
+//Relational Opreators:-
+        // ==, <= , >= ,< ,> , !=
+
+//Logical Opreators:-
+        // && (and) , || (OR), ! (NOT)
+
+//Assigment Opreators:-
+        //=, += ,-=, *=,/=
+    }
+}
