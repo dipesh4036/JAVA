a05c5d0e6316eb535520ef43b5f3a1a5fd0311a3
dipesh4036 <111883699+dipesh4036@users.noreply.github.com>
2023-10-08 11:49:58 +0000

    main

--- Basic-1/04_loops.java
+++ Basic-1/04_loops.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 class main{
     public static void main(String[] args) {
-        
+
+//While loops
+        // Scanner sc= new Scanner(System.in);
+        // System.out.println("Enter your number:-");
+        // int n = sc.nextInt();
+        // int i = 1;
+        // int sum=0;
+        // while(i<=n){
+        //     sum=sum+i;
+        //     i++;   
+        // }
+        // System.out.println(sum);
+
+//For loops
+
+        //     int i= 12345;
+        //     int rev=0;
+        //     for (int j = 0; j < i; j++) {
+        //         int last=i%10;
+        //         rev=(rev*10)+last;
+        //         i=i/10;
+        //     }
+        //    System.out.println(rev);
+
+//Enter 10 multiples and stop
+        // Scanner sc=new Scanner(System.in);
+        // while (true) {
+        //     System.out.println("enter your number: ");
+        //     int n= sc.nextInt();
+        //     if (n%10==0) {
+        //         break;
+        //     }
+        // }
     }
 }
