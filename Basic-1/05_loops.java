87f3ca78865936c3789ffdaff95b53b5a05820c3
dipesh4036 <111883699+dipesh4036@users.noreply.github.com>
2023-10-09 09:06:44 +0000

    main

---
+++ Basic-1/05_loops.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class loops{
+    public static void main(String[] args) {
+
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+// Star Pattern
+
+        // for (int i = 0; i <=n; i++) {
+        //     for (int j = 0; j < i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println("----------------------------");
+
+        // for (int i = 0; i <=n; i++) {
+        //     for (int j = 0; j < n-i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println("----------------------------");
+
+        //  for (int i = 1; i <=n; i++) {
+        //     for (int j = 1; j <i+1; j++) {
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        //  }
+
+        //  System.out.println("----------------------------");
+
+        // char ch = 'A';
+        //  for (int i = 1; i <=n; i++) {
+        //     for (int j = 1; j <i+1; j++) {
+        //         System.out.print(ch);
+        //         ch++;
+        //     }
+        //     System.out.println();
+        //  }
+    }
+}
