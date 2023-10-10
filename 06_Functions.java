ef5f9a9a8c6f0e3b8ffc17a31bb7f6999323912d
dipesh4036 <111883699+dipesh4036@users.noreply.github.com>
2023-10-10 08:46:44 +0000

    main

---
+++ 06_Functions.java
@@ -0,0 +1,24 @@
+class index{
+    public static int sum(int a, int b){   //parameter or formal value
+        int c = a+b;
+        System.out.println(c);
+        return c;
+    }
+    static void changeA(int a){
+        int b=10;
+        System.out.println(b);
+        return;
+    }
+    public static void main(String[] args) {
+        
+// Functions in Java:-
+
+    //    sum(4, 5);  // arguments or actual value
+    int a=5;
+    changeA(a);
+    System.out.println(a);
+    }
+
+//What is Call by value and call by Reference:-  JAVA is always call by value.
+
+}
