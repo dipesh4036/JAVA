830deaee23ffa8840964daf2ddd5d65ac8c93b5b
dipesh4036 <111883699+dipesh4036@users.noreply.github.com>
2023-10-10 10:27:53 +0000

    main

--- Basic-1/06_Functions.java
+++ Basic-1/06_Functions.java
@@ -1,24 +1,85 @@
-class index{
-    public static int sum(int a, int b){   //parameter or formal value
-        int c = a+b;
-        System.out.println(c);
-        return c;
-    }
-    static void changeA(int a){
-        int b=10;
-        System.out.println(b);
-        return;
-    }
+class 06_Function{
+    // public static int sum(int a, int b){   //parameter or formal value
+    //     int c = a+b;
+    //     System.out.println(c);
+    //     return c;
+    // }
+
+    // static void changeA(int a){
+    //     int b=10;
+    //     System.out.println(b);
+    //     return;
+
+    // static int  fact(int n){
+    //     int f=1;
+    //     for (int i = 1; i <=n; i++) {
+    //         f=f*i;
+    //     }
+    //     return f;
+    // }
+
+    // static void bio(int n,int r){
+    //     int a = fact(n);
+    //     int b = fact(r);
+    //     int c = fact(n-r);
+    //     int biono= a/(b*c);
+    //     System.out.println(biono);
+    // }
+
+    // static boolean prime(int n){
+    //     if (n==2) {
+    //         return true;
+    //     }
+    //     for (int i = 2; i <= Math.sqrt(n); i++) {
+    //         if (n%i==0) {
+    //            return false;
+    //         }
+    //     }
+    //     return true;
+
+    // }
+
+    // static void rangeprime(int n){
+    //     for (int i = 2; i <=n; i++) {
+    //         if (prime(i)==true) {
+    //             System.out.print(i+" ");
+    //         }
+    //     }
+    // }
+
     public static void main(String[] args) {
         
 // Functions in Java:-
 
     //    sum(4, 5);  // arguments or actual value
-    int a=5;
-    changeA(a);
-    System.out.println(a);
-    }
 
 //What is Call by value and call by Reference:-  JAVA is always call by value.
- sout
+
+    // int a=5;
+    // changeA(a);
+    // System.out.println(a);
+
+// Factorial of an number:-
+
+        // int a=4;
+        // fact(a);
+
+//Binomial coefficient
+
+        // int n = 4;
+        // int r = 2;
+        // bio(n,r);
+
+//Function overloading:- same name function  diff parameter.
+
+        // 1) using parameter 
+        // 2) using data type
+    
+// prime or not 
+
+    //     int n=20;
+    //     rangeprime(n);
+
+    }
+
 }
