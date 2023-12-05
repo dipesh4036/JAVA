489cc685fcdd4ef722f136ba263e3d51a43aea6d
dipesh4036 <111883699+dipesh4036@users.noreply.github.com>
2023-12-05 13:08:11 +0000

    main

--- Arrays/array.java
+++ Arrays/array.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class array {
     public static void main(String[] args) {
-        System.out.println("asd");
+// creating array
+        int marks[]=new int [50];
+        int number[]={1,2,3};
+        String name[]={"dipesh","rahul","vimal","milan"};
+
+        Scanner sc = new Scanner(System.in);
+        int marks[0]=sc.ne
+        
+
+
     }
     
 }
